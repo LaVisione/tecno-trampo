a77cd3b8f4f72d5ca2687e1f8167804ffbc3da14
DY4M3S <dyamespdu@gmail.com>
2024-10-26 17:13:59 -0300

    "Ajuste na atualização de status do Banco"

--- monitorador_de_webservices_bancarios_backend/src/services/BankServices.ts
+++ monitorador_de_webservices_bancarios_backend/src/services/BankServices.ts
@@ -3,14 +3,20 @@ import { RepositoryBank } from "../repository/RepositoryBank";
 const repository = new RepositoryBank();
 
 export class BankServices {
-    
+
     public updateStatus(id: number, status: string) {
-        console.log("\nCHEGOU NA BANK-SERVICE");
-        repository.updateById(id,status);
+        console.log("\nCHEGOU NA BANK-SERVICE", status);
+        status = this.formatStatus(status);
+        repository.updateById(id, status);
     }
 
-   /*  private formatStatus(status:string) {
-        if (status == )
-    } */
+    private formatStatus(status: string) {
+        const statusCode = Number(status);
 
+        if (statusCode >= 100 && statusCode <= 499) {
+            return 'Online';
+        } else {
+            return 'Offline';
+        }
+    }
 }
